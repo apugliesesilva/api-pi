2efcd479d232b0f57e3d8514198c7493bbda7ee6
Athos <athospugliesedev@gmail.com>
2024-03-29 21:21:59 -0300

    new insight request

--- src/middlewares/verify-user-role.ts
+++ src/middlewares/verify-user-role.ts
@@ -3,13 +3,13 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export function verifyUserRole(roleToVerify: 'ADMIN' | 'STUDENT') {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     if (!request.user || typeof request.user !== 'object') {
-      return reply.status(401).send({ message: 'Unauthorized.' });
+      return reply.status(401).send({ message: 'Não autorizado 1.' });
     }
 
     const { role } = request.user as { role: 'ADMIN' | 'STUDENT' };
 
     if (role !== roleToVerify) {
-      return reply.status(401).send({ message: 'Não autorizado.' });
+      return reply.status(401).send({ message: 'Não autorizado 2.' });
     }
   }
 }
